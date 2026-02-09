ab01ce271b86099a7c507307cd4cb80aa0d37631
devpyc <yechan6855@naver.com>
2026-02-09 22:07:58 +0900

    feat: 수동 오프셋 방식 -> 동적 변수 선언 방식 전환

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 class CodeGenerator {
     ofstream out;
+    map<string,int>variableOffsets;
+    int currentOffset=0;
 
 public:
     CodeGenerator(string filename) {
@@ -20,6 +22,24 @@ public:
         out<<"  push rbp\n";
         out<<"  mov rbp, rsp\n";
         out<<"  sub rsp, 32\n";
+    }
+
+    void declare(string name, int value) {
+        currentOffset+=4;
+        variableOffsets[name]=currentOffset;
+        out<<"  mov DWORD PTR [rbp-"<<currentOffset<<"], "<<value<<"    #"<<name<<" = "<<value<<"\n";
+    }
+
+    void add(string dest, string lhs, string rhs) {
+        int lhsOffset=variableOffsets[lhs];
+        int rhsOffset=variableOffsets[rhs];
+
+        currentOffset+=4;
+        variableOffsets[dest]=currentOffset;
+
+        out<<"  mov eax, DWORD PTR [rbp-"<<lhsOffset<<"]\n";
+        out<<"  add eax, DWORD PTR [rbp-"<<rhsOffset<<"]\n";
+        out<<"  mov DWORD PTR [rbp-"<<currentOffset<<"], eax    # "<<dest<<" = "<<lhs<<" + "<<rhs<<"\n";
     }
 
     void assign(int offset, int value) {
@@ -41,24 +61,31 @@ int main() {
     // 1. Compile Start
     gen.start();
 
-    // 2. Variable Declare
-    gen.assign(4,10); // save from rbp-4
-    gen.assign(8,20); // save from rbp-8
+    // 2-1. Integer Variable Declare
+    /*
+     *gen.declare(4,10); // save from rbp-4
+    gen.declare(8,20); // save from rbp-8
+    */
+
+    // 2-2. Variable Name Declare
+    gen.declare("a",10);
+    gen.declare("b",20);
+
+    gen.add("c","a","b");
 
     // 3. Compile End
     gen.end();
 
     // 4. g++ build
     cout<<"Assembly file generated: "<<asmFile<<endl;
-    string compileCmd="g++ "+asmFile+" -o main";
+    string compileCmd="g++ "+asmFile+" -o my_program";
 
     int result=system(compileCmd.c_str());
 
     if (result==0) {
-        cout<<"Build Success! Run './main'"<<endl;
+        cout<<"Build Success! Run './my_program'"<<endl;
     } else {
         cout<<"Build Failed."<<endl;
     }
-
     return 0;
 }

4be4c517ff196817bb29040c68ad7ec93afc72fc
devpyc <yechan6855@naver.com>
2026-02-10 21:50:21 +0900

    feat: 어셈블리 내의 printf 호출을 통한 변수 출력 기능 추가

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 class CodeGenerator {
     ofstream out;
-    map<string,int>variableOffsets;
-    int currentOffset=0;
+    map<string,int>variableOffsets; // 변수 이름 -> 스택 위치
+    int currentOffset=0; // 사용된 스택
 
 public:
     CodeGenerator(string filename) {
@@ -21,15 +21,17 @@ public:
         out<<"main:\n";
         out<<"  push rbp\n";
         out<<"  mov rbp, rsp\n";
-        out<<"  sub rsp, 32\n";
+        out<<"  sub rsp, 32\n"; // 지역 변수를 위한 공간 확보
     }
 
+    // 1. 변수 선언: 문자열을 주면 자동으로 오프셋 할당하기
     void declare(string name, int value) {
-        currentOffset+=4;
+        currentOffset+=4; // int 크기만큼 이동
         variableOffsets[name]=currentOffset;
         out<<"  mov DWORD PTR [rbp-"<<currentOffset<<"], "<<value<<"    #"<<name<<" = "<<value<<"\n";
     }
 
+    // 2. 변수 덧셈 연산 (dest = lhs + rhs)
     void add(string dest, string lhs, string rhs) {
         int lhsOffset=variableOffsets[lhs];
         int rhsOffset=variableOffsets[rhs];
@@ -37,6 +39,7 @@ public:
         currentOffset+=4;
         variableOffsets[dest]=currentOffset;
 
+        // 메모리끼리의 연산이 불가능하므로 eax 레지스터 이용
         out<<"  mov eax, DWORD PTR [rbp-"<<lhsOffset<<"]\n";
         out<<"  add eax, DWORD PTR [rbp-"<<rhsOffset<<"]\n";
         out<<"  mov DWORD PTR [rbp-"<<currentOffset<<"], eax    # "<<dest<<" = "<<lhs<<" + "<<rhs<<"\n";
@@ -61,16 +64,12 @@ int main() {
     // 1. Compile Start
     gen.start();
 
-    // 2-1. Integer Variable Declare
-    /*
-     *gen.declare(4,10); // save from rbp-4
-    gen.declare(8,20); // save from rbp-8
-    */
-
-    // 2-2. Variable Name Declare
+    // 2. Variable Name Declare
+    // 수동으로 오프셋을 제어하던 기존 방식에서 변수 이름으로 제어하도록 변경
     gen.declare("a",10);
     gen.declare("b",20);
 
+    // a와 b를 더해서 c에 저장하는 명령어 생성
     gen.add("c","a","b");
 
     // 3. Compile End

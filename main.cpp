660a719bffe42eeebb3d915d01e0d0482569e302
devpyc <yechan6855@naver.com>
2026-02-10 22:03:41 +0900

    fix: 변수 출력 기능 누락 확인 후 수정

--- main.cpp
+++ main.cpp
@@ -25,14 +25,14 @@ public:
     }
 
     // 1. 변수 선언: 문자열을 주면 자동으로 오프셋 할당하기
-    void declare(string name, int value) {
+    void declare(string name, int value=0) {
         currentOffset+=4; // int 크기만큼 이동
         variableOffsets[name]=currentOffset;
         out<<"  mov DWORD PTR [rbp-"<<currentOffset<<"], "<<value<<"    #"<<name<<" = "<<value<<"\n";
     }
 
-    // 2. 변수 덧셈 연산 (dest = lhs + rhs)
-    void add(string dest, string lhs, string rhs) {
+    // 2. 변수 산술 연산 (add, sub, mul)
+    void arithmetic(string op, string dest, string lhs, string rhs) {
         int lhsOffset=variableOffsets[lhs];
         int rhsOffset=variableOffsets[rhs];
 
@@ -41,12 +41,30 @@ public:
 
         // 메모리끼리의 연산이 불가능하므로 eax 레지스터 이용
         out<<"  mov eax, DWORD PTR [rbp-"<<lhsOffset<<"]\n";
-        out<<"  add eax, DWORD PTR [rbp-"<<rhsOffset<<"]\n";
-        out<<"  mov DWORD PTR [rbp-"<<currentOffset<<"], eax    # "<<dest<<" = "<<lhs<<" + "<<rhs<<"\n";
+        if (op == "+") out<<"  add eax, DWORD PTR [rbp-"<<rhsOffset<<"]\n";
+        else if (op == "-") out<<"  sub eax, DWORD PTR [rbp-"<<rhsOffset<<"]\n";
+        else if (op == "*") out<<"  imul eax, DWORD PTR [rbp-"<<rhsOffset<<"]\n";
+    }
+    
+    // 3. 변수 입력 기능
+    void inputVariable(string name){
+        int offset=variableOffsets[name];
+        out<<"  # scanf(\"%d\", &"<<name<<")\n";
+        out<<"  lea rdi, [rip + fmt_in]\n";
+        out<<"  lea rsi, [rbp-"<<offset<<"] # 변수의 주소를 전달\n";
+        out<<"  mov eax, 0\n";
+        out<<"  call scanf\n";
     }
 
     void assign(int offset, int value) {
         out<<"  mov DWORD PTR [rbp-"<<offset<<"], "<<value<<"\n";
+    }
+
+    // 4. 변수 출력 기능
+    void printVariable(string name){
+        out<<"  lea rdi, [rip+fmt_out]\n";
+        out<<"  mov esi, DWORD PRT [rbp-"<<variableOffsets[name]<<"]\n";
+        out<<"  mov eax, 0\n call pritnf\n";
     }
 
     void end() {
@@ -66,11 +84,18 @@ int main() {
 
     // 2. Variable Name Declare
     // 수동으로 오프셋을 제어하던 기존 방식에서 변수 이름으로 제어하도록 변경
-    gen.declare("a",10);
-    gen.declare("b",20);
+    gen.declare("a");
+    gen.declare("b");
 
-    // a와 b를 더해서 c에 저장하는 명령어 생성
-    gen.add("c","a","b");
+    // 연산 수행: t1 = a - b
+    gen.arithmetic("-","t1","a","b")
+
+    // 상수 곱셈을 위한 임시 변수
+    gen.declare("two",2);
+    gen.arithmetic("*","result","t1","two");
+    
+    // 결과 출력
+    gen.printVariable("result");
 
     // 3. Compile End
     gen.end();
